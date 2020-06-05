32ac9adb8dbf5df74e886cb6031527ef663f37d6
nvetrov@gmail.com <C1vmdpalc33>
2020-06-05 21:22:12 +0300

    done!

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from common.models import UserProfile
+
 
 # Register your models here.
+
+
+# Register your models here.
+@admin.register(UserProfile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
---
+++ common/urls.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.views import LoginView, LogoutView
+
+from common.views import index, CreateUserProfile, RegisterView
+# , login, logout
+from django.urls import path, reverse_lazy
+
+app_name = 'common'
+urlpatterns = [
+    path('', index, name='index'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(template_name='register.html',
+                                           success_url=reverse_lazy('common:profile-create')),
+         name='register'),
+    path('profile-create/', CreateUserProfile.as_view(), name='profile-create'),
+]
